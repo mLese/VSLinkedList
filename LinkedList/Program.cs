11d2ad277042abdd8c6f14a6020134c153769556
Michael Lese <michael.lese@cincpro.com>
2019-07-19 21:39:45 -0400

    Linked List Exercise

---
+++ LinkedList/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+
+    interface ILinkedList<T>
+    {
+        T getFirst();
+        T getLast();
+        T removeFirst();
+        T removeLast();
+        void addFirst(T val);
+        void addLast(T val);
+        void remove(T val);
+        int size();
+        void clear();
+    }
+
+    class MyNode<T>
+    {
+        T value;
+        MyNode<T> next;
+
+        MyNode(T value) {
+            this.value = value;
+            next = null;
+        }
+    }
+
+    public class MyLinkedList<T> : ILinkedList<T>
+    {
+        public void addFirst(T val)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void addLast(T val)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T getFirst()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T getLast()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void remove(T val)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T removeFirst()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T removeLast()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int size()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ LinkedListTests/MyLinkedListTests.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LinkedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList.Tests
+{
+    [TestClass()]
+    public class MyLinkedListTests
+    {
+        MyLinkedList<String> list;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            list = new MyLinkedList<String>();
+        }
+
+        [TestMethod()]
+        public void addFirstTest()
+        {
+            list.addFirst("string1");
+            Assert.AreEqual("string1", list.getFirst());
+    
+        }
+
+        [TestMethod()]
+        public void addLastTest()
+        {
+            list.addFirst("string1");
+            list.addLast("string2");
+            Assert.AreEqual("string2", list.getLast());
+        }
+
+        [TestMethod()]
+        public void clearTest()
+        {
+            list.addFirst("string1");
+            list.addFirst("string2");
+            list.clear();
+            Assert.AreEqual(0, list.size());
+        }
+
+        [TestMethod()]
+        public void removeTest()
+        {
+            list.addFirst("string1");
+            list.addFirst("string2");
+            list.addFirst("string3");
+            list.remove("string2");
+            Assert.AreEqual("string1", list.getFirst());
+            Assert.AreEqual("string3", list.getLast());
+        }
+
+        [TestMethod()]
+        public void removeFirstTest()
+        {
+            list.addFirst("string1");
+            list.addFirst("string2");
+            list.removeFirst();
+            Assert.AreEqual("string1", list.getFirst());
+        }
+
+        [TestMethod()]
+        public void removeLastTest()
+        {
+            list.addFirst("string1");
+            list.addLast("string2");
+            list.removeLast();
+            Assert.AreEqual("string1", list.getLast());
+        }
+
+        [TestMethod()]
+        public void sizeTest()
+        {
+            list.addFirst("string1");
+            list.addFirst("string2");
+            list.addFirst("string3");
+            Assert.AreEqual(3, list.size());
+        }
+    }
+}
